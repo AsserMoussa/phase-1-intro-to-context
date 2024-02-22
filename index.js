de2b6fba19872ed7030e7ac9b44b8c5d6273bdde
assermoussa <asser.y.moussa@gmail.com>
2024-02-21 18:36:34 -0600

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,80 @@
-// Your code here
+
+
+function createEmployeeRecord([fN, faN, ti, pay]){
+    const emp = {
+        firstName: fN,
+        familyName: faN,
+        title: ti,
+        payPerHour: pay,
+        timeInEvents: [],
+        timeOutEvents: []
+      };
+      
+      return emp;
+}
+
+function createEmployeeRecords(emps){
+    let newArr = [];
+    emps.forEach(element => {
+        newArr.push(createEmployeeRecord(element))  
+});
+return newArr;
+}
+
+function createTimeInEvent(emp, stamp){
+    //console.log(stamp.split(" "));
+let splitStamp = stamp.split(" ");
+    let timeInObj = {
+    type: "TimeIn",
+    hour: parseInt(splitStamp[1]),
+    date: splitStamp[0]
+}
+emp.timeInEvents.push(timeInObj);
+return emp;
+}
+
+function createTimeOutEvent(emp, stamp){
+    //console.log(stamp.split(" "));
+let splitStamp = stamp.split(" ");
+    let timeOutObj = {
+    type: "TimeOut",
+    hour: parseInt(splitStamp[1]),
+    date: splitStamp[0]
+}
+emp.timeOutEvents.push(timeOutObj);
+return emp;
+}
+
+function hoursWorkedOnDate(emp, stamp){
+let timeInEvent = emp.timeInEvents.find(function(element){
+    return element.date== stamp;
+
+})
+let timeOutEvent = emp.timeOutEvents.find(function(element){
+    return element.date== stamp;
+})
+return (timeOutEvent.hour - timeInEvent.hour)/100;
+}
+
+function wagesEarnedOnDate(emp, stamp){
+return emp.payPerHour*hoursWorkedOnDate(emp, stamp);
+}
+
+function allWagesFor(emp){
+    let dates = emp.timeInEvents.map(function(element){
+        return element.date;
+    }) 
+    let total = 0;
+    for(let i=0;i<dates.length;i++){   
+total = total + wagesEarnedOnDate(emp, dates[i]); 
+    }
+    return total;
+}
+
+function calculatePayroll(emp){
+    let total = 0;
+    for(let i=0;i<emp.length;i++){   
+        total = total + allWagesFor(emp[i]); 
+            }
+            return total; 
+}
